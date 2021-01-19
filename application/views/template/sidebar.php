3018246ba29a82eff29e7f934f8e406a629f627a
danakristiawan <danakristiawan@gmail.com>
2021-01-19 07:10:01 +0700

    create level and menu array

--- application/views/template/sidebar.php
+++ application/views/template/sidebar.php
@@ -1,18 +1,41 @@
 <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
     <div class="position-sticky pt-3">
         <?php
-        $levels = $this->db->get('levels')->result_array();
+        $levels = [
+            ['id' => 1, 'level' => 'Halaman Utama'],
+            ['id' => 2, 'level' => 'Rincian'],
+            ['id' => 3, 'level' => 'Laporan']
+        ];
+        $menus = [
+            ['menu' => 'Beranda', 'level' => 1, 'url' => 'beranda'],
+            ['menu' => 'Penghasilan', 'level' => 1, 'url' => 'penghasilan'],
+            ['menu' => 'Gaji', 'level' => 2, 'url' => 'gaji'],
+            ['menu' => 'Kekurangan Gaji', 'level' => 2, 'url' => 'kekurangan-gaji'],
+            ['menu' => 'Uang Makan', 'level' => 2, 'url' => 'uang-makan'],
+            ['menu' => 'Uang Lembur', 'level' => 2, 'url' => 'uang-lembur'],
+            ['menu' => 'Tunjangan Kinerja', 'level' => 2, 'url' => 'tunjangan-kinerja'],
+            ['menu' => 'Pembayaran Lainnya', 'level' => 2, 'url' => 'pembayaran-lainnya'],
+            ['menu' => 'PPh Pasal 21', 'level' => 3, 'url' => 'pph-pasal-21'],
+            ['menu' => 'PPh Pasal 21 Final', 'level' => 3, 'url' => 'pph-pasal-21-final'],
+            ['menu' => 'Penghasilan Tahunan', 'level' => 3, 'url' => 'penghasilan-tahunan'],
+            ['menu' => 'Dokumen Perubahan', 'level' => 3, 'url' => 'dokumen-perubahan']
+        ];
         foreach ($levels as $r) :
             $level = $r['level'];
             $id_level = $r['id'];
+            $newAr = [];
+            foreach ($menus as $val) {
+                if ($val['level'] == $id_level) {
+                    $newAr[] = $val;
+                }
+            }
         ?>
             <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-3 mb-1 text-muted">
                 <span><?= $level; ?></span>
             </h6>
             <ul class="nav flex-column">
                 <?php
-                $menus = $this->db->get_where('menus', ['level' => $id_level])->result_array();
-                foreach ($menus as $s) :
+                foreach ($newAr as $s) :
                     $menu = $s['menu'];
                     $url = $s['url'];
                 ?>
